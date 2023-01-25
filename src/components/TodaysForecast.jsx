8fb58b094036c84f04c7ba3d15369cee860cf0c1
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2023-01-25 10:16:15 -0300

    Added todays forecast feature

--- src/components/TodaysForecast.jsx
+++ src/components/TodaysForecast.jsx
@@ -1,34 +1,56 @@
 import React from "react";
 
-const TodaysForecast = () => {
+const TodaysForecast = ({ data }) => {
+  console.log(data);
+  console.log(data.list[0].dt_txt.slice(11, 16));
   return (
     <div className="todays-forecast-container">
       <h3>Today's Forecast</h3>
       <div className="todays-forecast--hours">
         <div className="hours">
-          <h4 className="hours">06:00</h4>
-          <img className="climate-img" src={`icons/01d.png`} alt="" />
-          <p className="temperature">x°C</p>
+          <h4 className="hours">{data.list[0].dt_txt.slice(11, 16)}</h4>
+          <img
+            className="climate-img"
+            src={`icons/${data.list[0].weather[0].icon}.png`}
+            alt=""
+          />
+          <p className="temperature">{Math.round(data.list[0].main.temp)}°C</p>
         </div>
         <div className="hours">
-          <h4 className="hours">06:00</h4>
-          <img className="climate-img" src={`icons/01d.png`} alt="" />
-          <p className="temperature">x°C</p>
+          <h4 className="hours">{data.list[1].dt_txt.slice(11, 16)}</h4>
+          <img
+            className="climate-img"
+            src={`icons/${data.list[1].weather[0].icon}.png`}
+            alt=""
+          />
+          <p className="temperature">{Math.round(data.list[1].main.temp)}°C</p>
         </div>
         <div className="hours">
-          <h4 className="hours">06:00</h4>
-          <img className="climate-img" src={`icons/01d.png`} alt="" />
-          <p className="temperature">x°C</p>
+          <h4 className="hours">{data.list[2].dt_txt.slice(11, 16)}</h4>
+          <img
+            className="climate-img"
+            src={`icons/${data.list[2].weather[0].icon}.png`}
+            alt=""
+          />
+          <p className="temperature">{Math.round(data.list[2].main.temp)}°C</p>
         </div>
         <div className="hours">
-          <h4 className="hours">06:00</h4>
-          <img className="climate-img" src={`icons/01d.png`} alt="" />
-          <p className="temperature">x°C</p>
+          <h4 className="hours">{data.list[3].dt_txt.slice(11, 16)}</h4>
+          <img
+            className="climate-img"
+            src={`icons/${data.list[3].weather[0].icon}.png`}
+            alt=""
+          />
+          <p className="temperature">{Math.round(data.list[3].main.temp)}°C</p>
         </div>
         <div className="hours">
-          <h4 className="hours">06:00</h4>
-          <img className="climate-img" src={`icons/01d.png`} alt="" />
-          <p className="temperature">x°C</p>
+          <h4 className="hours">{data.list[4].dt_txt.slice(11, 16)}</h4>
+          <img
+            className="climate-img"
+            src={`icons/${data.list[4].weather[0].icon}.png`}
+            alt=""
+          />
+          <p className="temperature">{Math.round(data.list[4].main.temp)}°C</p>
         </div>
       </div>
     </div>
